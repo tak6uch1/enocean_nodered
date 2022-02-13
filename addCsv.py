8282373f1c70a78319c507d6a37e1ce073617b3c
Shinya Takeuchi <tak6uch1@gmail.com>
2022-02-14 00:25:35 +0900

    Add addCsv.py

---
+++ addCsv.py
@@ -0,0 +1,17 @@
+import re
+import datetime
+import subprocess
+
+passwd = 'PASSWORD\n'.encode()
+
+s = str(input())
+s = re.sub('^.*: *', '', s)
+s = re.sub(' *}$', '', s)
+
+if int(s) > 0:
+    dt_now = datetime.datetime.now()
+    dt_str = dt_now.strftime('%Y/%m/%d %H:%M:%S')
+    out_str = dt_str + "," + s + "\n"
+    with open('/home/pi/work/EnOcean/button.csv', mode='a') as f:
+        f.write(out_str)
+        subprocess.run(('sudo', '-S', 'cp', '/home/pi/work/EnOcean/button.csv', '/srv/dev-disk-by-uuid-57c7c9da-d3c7-4f6c-82ab-52a5fa017821/nas_user/nas_user/'), input=passwd, check=True)
